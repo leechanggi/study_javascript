bf7810a9de5fc5ecfc46947c418d3d44c1519f8a
leechanggi <67743720+leechanggi@users.noreply.github.com>
2021-05-26 18:53:24 +0900

    summary
    2021/05/26.18:53

--- practice4/js/main.js
+++ practice4/js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+const input = document.querySelector('#textInput');
+const shopListWrap = document.querySelector('.shopList-wrap');
+let inputVal = input.value;
+let titleText = document.querySelector('.listTitle');
+function listAddFnc() {
+    const shopListadd = document.createElement('li');
+    shopListadd.setAttribute('class','shopList');
+    shopListWrap.appendChild(shopListadd);
+    let shopList = document.querySelector('.shopList');
+    shopList.innerHTML = `<span class="listTitle">${inputVal = input.value}</span>
+    <span class="listDel">
+    <button class="btnDel" type="button"></button>
+    </span>`;
+}
+input.addEventListener('keyup', (e)=> {
+    if(e.keyCode === 13){
+        listAddFnc();
+    } else{
+        return false;
+    }
+});
+// const shopListWrap = document.querySelector('.shopList-wrap');
+// const shopListadd = shopListWrap.createElement('li');
+// shopListadd.setAttribute('class','shopList');
+// const shopList = document.querySelector('.shopList');
+// shopList.innerHTML = `<span class="listTitle">우유</span>
+// <span class="listDel">
+//     <button class="btnDel" type="button"></button>
+// </span>`;
